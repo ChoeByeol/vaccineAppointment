b60d85e3e47fa580bfc16d33ce51fb64f77fc65e
chany <feeble19@naver.com>
2021-11-09 19:22:36 +0900

    모르겠다..

--- semiTest/src/main/java/kh8semi1.beans/VaccineDto.java
+++ semiTest/src/main/java/kh8semi1.beans/VaccineDto.java
@@ -1,4 +1,4 @@
-package project1.beans;
+package kh8semi1.beans;
 
 public class VaccineDto {
 	
@@ -8,7 +8,7 @@ public class VaccineDto {
 	private String vaccineValue;
 	private String vaccineCompany;
 	private String vaccineAge;
-	private String vaccineComposition; 
+	private String vaccineComposition;
 	private String vaccineInterval;
 	private String vaccineMethod;
 	private String vaccineKeep;
