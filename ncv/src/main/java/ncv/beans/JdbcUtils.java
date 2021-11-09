dbb233a5faf018faf76a160feb689fe0ea6c1a8d
Choe Byeol <cb68862643@gmail.com>
2021-11-09 20:51:43 +0900

    세미 프로젝트 기본 설정 파일

---
+++ ncv/src/main/java/ncv/beans/JdbcUtils.java
@@ -0,0 +1,31 @@
+package ncv.beans;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class JdbcUtils {
+
+	private static DataSource ds;
+	
+	static {
+		Context ctx;
+		try {
+			ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/oracle");
+		} 
+		catch (Exception e) {
+			System.err.println("DataSource 생성 오류");
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection connect() throws Exception{
+		return ds.getConnection();
+	}
+	
+}
---
+++ ncv/src/main/java/ncv/beans/VaccineDto.java
@@ -0,0 +1,94 @@
+package ncv.beans;
+
+public class VaccineDto {
+	
+	private int vaccineNo;
+	private String vaccinePlatform;
+	private String vaccineName;
+	private String vaccineValue;
+	private String vaccineCompany;
+	private String vaccineAge;
+	private String vaccineComposition;
+	private String vaccineInterval;
+	private String vaccineMethod;
+	private String vaccineKeep;
+	private String vaccineExpire;
+	private String vaccineLife;
+	
+	public VaccineDto() {
+		super();
+	}
+	public String getVaccinePlatform() {
+		return vaccinePlatform;
+	}
+	public void setVaccinePlatform(String vaccinePlatform) {
+		this.vaccinePlatform = vaccinePlatform;
+	}
+	public int getVaccineNo() {
+		return vaccineNo;
+	}
+	public void setVaccineNo(int vaccineNo) {
+		this.vaccineNo = vaccineNo;
+	}
+	public String getVaccineName() {
+		return vaccineName;
+	}
+	public void setVaccineName(String vaccineName) {
+		this.vaccineName = vaccineName;
+	}
+	public String getVaccineValue() {
+		return vaccineValue;
+	}
+	public void setVaccineValue(String vaccineValue) {
+		this.vaccineValue = vaccineValue;
+	}
+	public String getVaccineCompany() {
+		return vaccineCompany;
+	}
+	public void setVaccineCompany(String vaccineCompany) {
+		this.vaccineCompany = vaccineCompany;
+	}
+	public String getVaccineAge() {
+		return vaccineAge;
+	}
+	public void setVaccineAge(String vaccineAge) {
+		this.vaccineAge = vaccineAge;
+	}
+	public String getVaccineComposition() {
+		return vaccineComposition;
+	}
+	public void setVaccineComposition(String vaccineComposition) {
+		this.vaccineComposition = vaccineComposition;
+	}
+	public String getVaccineInterval() {
+		return vaccineInterval;
+	}
+	public void setVaccineInterval(String vaccineInterval) {
+		this.vaccineInterval = vaccineInterval;
+	}
+	public String getVaccineMethod() {
+		return vaccineMethod;
+	}
+	public void setVaccineMethod(String vaccineMethod) {
+		this.vaccineMethod = vaccineMethod;
+	}
+	public String getVaccineKeep() {
+		return vaccineKeep;
+	}
+	public void setVaccineKeep(String vaccineKeep) {
+		this.vaccineKeep = vaccineKeep;
+	}
+	public String getVaccineExpire() {
+		return vaccineExpire;
+	}
+	public void setVaccineExpire(String vaccineExpire) {
+		this.vaccineExpire = vaccineExpire;
+	}
+	public String getVaccineLife() {
+		return vaccineLife;
+	}
+	public void setVaccineLife(String vaccineLife) {
+		this.vaccineLife = vaccineLife;
+	}
+}
+
