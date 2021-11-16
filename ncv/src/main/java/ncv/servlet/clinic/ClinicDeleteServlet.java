ffe2c0a70e2148ff3d4a875e33de1deef9416b8a
Choe Byeol <cb68862643@gmail.com>
2021-11-16 11:07:50 +0900

    병원 정보 삭제 서블릿 추가

---
+++ ncv/src/main/java/ncv/servlet/clinic/ClinicDeleteServlet.java
@@ -0,0 +1,35 @@
+package ncv.servlet.clinic;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ncv.beans.ClinicDao;
+
+@WebServlet(urlPatterns="/clinic/delete.txt")
+public class ClinicDeleteServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			int clinicNo = Integer.parseInt(req.getParameter("clinicNo"));
+			
+			ClinicDao clinicDao = new ClinicDao();
+			boolean success = clinicDao.delete(clinicNo);
+			
+			if(success) {
+				resp.sendRedirect("list.txt");
+			}
+			else {
+				resp.sendError(404);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
+}
