1ae566d7fe94f4ed439164d47b04276cedc09240
Zaein-Kim <janelove333@gmail.com>
2021-11-16 16:47:53 +0900

    관리자 회원 수정 기능

--- ncv/src/main/java/ncv/sevlet/admin/AdminMemberEditServlet.java
+++ ncv/src/main/java/ncv/sevlet/admin/AdminMemberEditServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import ncv.beans.MemberDao;
 import ncv.beans.MemberDto;
 
-@WebServlet(urlPatterns = "/admin/member/edit.kh")
+@WebServlet(urlPatterns = "/admin/member/edit.txt")
 public class AdminMemberEditServlet extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -18,7 +18,10 @@ public class AdminMemberEditServlet extends HttpServlet{
 			//입력 : MemberDto()
 			
 			MemberDto memberDto = new MemberDto();
+			
 			memberDto.setMemberName(req.getParameter("memberName"));
+			memberDto.setMemberRrn(req.getParameter("memberRrn"));
+			memberDto.setMemberGender(req.getParameter("memberGender"));
 			memberDto.setMemberAddress(req.getParameter("memberAddress"));
 			memberDto.setMemberPhone(req.getParameter("memberPhone"));
 			memberDto.setMemberRole(req.getParameter("memberRole"));
