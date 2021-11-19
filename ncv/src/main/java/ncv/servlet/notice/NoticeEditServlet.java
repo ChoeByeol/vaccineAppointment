dd77df2da2c29a751e42e7f0ea26fb8519910d0d
mjKim <rladbrud7633@naver.com>
2021-11-19 12:23:25 +0900

    notice 서블릿 및 빈즈 보충

---
+++ ncv/src/main/java/ncv/servlet/notice/NoticeEditServlet.java
@@ -0,0 +1,42 @@
+package ncv.servlet.notice;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ncv.beans.NoticeDao;
+import ncv.beans.NoticeDto;
+
+@WebServlet(urlPatterns = "/notice/edit.txt")
+public class NoticeEditServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			//입력
+			NoticeDto noticeDto = new NoticeDto();
+			noticeDto.setNoticeNo(Integer.parseInt(req.getParameter("noticeNo")));
+			noticeDto.setNoticeTitle(req.getParameter("noticeTitle"));
+			noticeDto.setNoticeContent(req.getParameter("noticeContent"));
+			
+			//처리
+			NoticeDao noticeDao = new NoticeDao();
+			boolean success = noticeDao.edit(noticeDto);
+			
+			//출력 
+			if(success) {
+				resp.sendRedirect("/notice/detail.jsp?noticeNo="+noticeDto.getNoticeNo());
+			}
+			else {
+				resp.sendError(404);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
+}
